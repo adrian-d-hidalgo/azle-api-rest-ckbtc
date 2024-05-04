2b1e3d5262666d6ece505f9de12dc59405fd26b0
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2024-05-04 00:35:11 -0600

    chore: Update dependencies and clean up project files

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -18,69 +18,66 @@ type User = {
 }
 
 let users: User[] = [];
+const app = express();
 
-export default Server(() => {
-    const app = express();
+app.use(express.json());
 
-    app.use(express.json());
+// Init ckBTC canisters
+const ckbtcLedger = new CkbtcLedger(
+    Principal.fromText(process.env.CKBTC_LEDGER_CANISTER_ID!)
+);
+const ckbtcMinter = new CkbtcMinter(
+    Principal.fromText(process.env.CKBTC_MINTER_CANISTER_ID!)
+);
 
-    // Init ckBTC canisters
-    const ckbtcLedger = new CkbtcLedger(
-        Principal.fromText(process.env.CKBTC_LEDGER_CANISTER_ID!)
-    );
-    const ckbtcMinter = new CkbtcMinter(
-        Principal.fromText(process.env.CKBTC_MINTER_CANISTER_ID!)
-    );
-
-    app.get('/users', (req, res) => {
-        res.json(users);
-    });
-
-    app.post('/users', async (req: Request<any, any, { username: string }>, res: Response) => {
-        const id = generateId();
-        const address = await ckbtcMinter.getAddress(id);
-        const user = {
-            id: id.toString(),
-            username: req.body.username,
-            address
-        };
-
-        users = [
-            ...users,
-            user,
-        ];
-
-        res.status(200);
-        res.send(user);
-    });
-
-    app.get('/users/:id/balance', async (req: Request<{ id: string }>, res: Response) => {
-        const principal = Principal.fromText(req.params.id);
-        const balance = await ckbtcLedger.getBalance(principal);
-
-        res.json({ balance });
-    });
-
-    app.put('/users/:id/balance', async (req: Request<{ id: string }>, res: Response) => {
-        const principal = Principal.fromText(req.params.id);
-        await ckbtcMinter.updateBalance(principal);
-
-        res.status(204);
-        res.send();
-    });
-
-    app.post('/transfer', async (req: Request<any, any, { from: string, to: string, amount: number }>, res: Response) => {
-        const { from, to, amount } = req.body;
-
-        await ckbtcLedger.transfer(
-            Principal.fromText(from),
-            Principal.fromText(to),
-            amount
-        );
-
-        res.status(204);
-        res.send();
-    });
-
-    return app.listen();
+app.get('/users', (req, res) => {
+    res.json(users);
 });
+
+app.post('/users', async (req: Request<any, any, { username: string }>, res: Response) => {
+    const id = generateId();
+    const address = await ckbtcMinter.getAddress(id);
+    const user = {
+        id: id.toString(),
+        username: req.body.username,
+        address
+    };
+
+    users = [
+        ...users,
+        user,
+    ];
+
+    res.status(200);
+    res.send(user);
+});
+
+app.get('/users/:id/balance', async (req: Request<{ id: string }>, res: Response) => {
+    const principal = Principal.fromText(req.params.id);
+    const balance = await ckbtcLedger.getBalance(principal);
+
+    res.json({ balance });
+});
+
+app.put('/users/:id/balance', async (req: Request<{ id: string }>, res: Response) => {
+    const principal = Principal.fromText(req.params.id);
+    await ckbtcMinter.updateBalance(principal);
+
+    res.status(204);
+    res.send();
+});
+
+app.post('/transfer', async (req: Request<any, any, { from: string, to: string, amount: number }>, res: Response) => {
+    const { from, to, amount } = req.body;
+
+    await ckbtcLedger.transfer(
+        Principal.fromText(from),
+        Principal.fromText(to),
+        amount
+    );
+
+    res.status(204);
+    res.send();
+});
+
+app.listen();
